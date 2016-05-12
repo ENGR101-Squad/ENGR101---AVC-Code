7b509647fb1565d7e2db768a40c287139997168b
Jacob <jacob.chn.gell@gmail.com>
2016-05-12 16:32:06 +1200

    Updated class, messing around wth values

--- robot_move.cpp
+++ robot_move.cpp
@@ -1,11 +1,43 @@
+#include <stdio.h>
+
+//#include <pthread.h>
+#include <time.h>
+#include <string.h>
+
+// sudo gcc -Wall
 extern "C" int init_hardware();
 extern "C" int init(int d_lev);
 
+extern "C" int take_picture();
+extern "C" char get_pixel(int row, int col, int color);
+extern "C" void set_pixel(int col, int row, char red,char green,char blue);
+
+extern "C" int open_screen_stream();
+extern "C" int close_screen_stream();
+extern "C" int update_screen();
+//extern "C" void GetLine(int row,int threshold);
+extern "C" int display_picture(int delay_sec,int delay_usec);
+extern "C" int save_picture(char filename[5]);
+
 extern "C" int set_motor(int motor,int speed);
 
+extern "C" int read_analog(int ch_adc);
+extern "C" int Sleep(int sec, int usec);
+extern "C" int select_IO(int chan, int direct);
+extern "C" int write_digital(int chan,char level);
+extern "C" int read_digital(int chan);
+extern "C" int set_PWM(int chan, int value);
+
+extern "C" int connect_to_server( char server_addr[15],int port);
+extern "C" int send_to_server(char message[24]);
+extern "C" int receive_from_server(char message[24]);
+
 int main() {
-    for(int i = 0; i < 20; i++) {
-        set_motor(0, 127);
-        set_motor(1,127);
-    }
+    init(0);
+    //Left is 1, right is 2
+    set_motor(1,127);
+    set_motor(2,127);
+    Sleep(3,0);
+    set_motor(1,0);
+    set_motor(2,0);
 }
