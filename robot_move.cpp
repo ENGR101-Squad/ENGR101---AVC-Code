d4c0187c34e0165e85da19e2e9fc4a8d91580387
raaghavt <raaghav26@gmail.com>
2016-05-12 16:19:29 +1200

    Update robot_move.cpp
    Added Comment

--- robot_move.cpp
+++ robot_move.cpp
@@ -5,6 +5,7 @@ extern "C" int set_motor(int motor,int speed);
 
 int main() {
     for(int i = 0; i < 20; i++) {
+        //do both motors run at the same speed? Or will we need to change one of the values?
         set_motor(0, 127);
         set_motor(1,127);
     }
